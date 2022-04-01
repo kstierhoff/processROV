553fa8ab10c7673f2922b98e4c86e009511c7d9f
Kevin Stierhoff <kevins@ucsc.edu>
2022-04-01 16:58:55 -0700

    Updated after processing 2109 SCI Abalone survey.

--- Doc/settings/settings_2019_SanClemente.R
+++ Doc/settings/settings_2019_SanClemente.R
@@ -1,4 +1,10 @@
 # User-defined variables -------------------------------------------------------
+# Processing controls
+copy.files        <- F # Copy files from root directories
+process.files     <- T # Process files
+process.files.all <- T # Process all files, not only new ones
+process.photos    <- T # Process photos
+
 # Process NAV date from DAT files ####
 if (.Platform$OS.type == "unix") { 
   # if working on a Mac
@@ -9,9 +15,10 @@ if (.Platform$OS.type == "unix") {
   dat.root 	<- "C:/ROV_DATA/NAVDATA" # This is the WinFrog project directory
   photo.root 	<- "C:/ROV_DATA/PHOTOS"#  This is the photo directory
 }
+
 # ROV database information
 db.source <- "Access" # Access or SQL
-db.dir    <- here("Data", "ROV_atsea_20191209.accdb")  
+db.dir    <- here("Data", "ROV_atsea_20220321.accdb")  
 
 # Dive information
 # If only processing one dive, dir.start and dir.end are the same
@@ -36,18 +43,6 @@ ROV <- "HDHV"
 # Fix photo date/time
 # If set correctly, use # '+="0:0:0 0:0:0"', else -=Y:M:D h:m:s
 timefix <- '+="0:0:0 0:0:0"' 
-
-# Navigation data information --------------------------------------------------
-# Process NAV date from DAT files ####
-if (.Platform$OS.type == "unix") { 
-  # if working on a Mac
-  dat.root 	<- "/Users/kevinstierhoff/NAVDATA" 	# This is the WinFrog project directory
-  photo.root 	<- "/Users/kevinstierhoff/PHOTOS" # This is the photo directory
-} else { 
-  # if working on a Windows PC
-  dat.root 	<- "C:/ROV_DATA/NAVDATA" # This is the WinFrog project directory
-  photo.root 	<- "C:/ROV_DATA/PHOTOS"#  This is the photo directory
-}
 
 # Interval for Loess smoother used to plot nav data
 nav.smoother <- 15
